5fd293e6fc2c59b48174ff772bf0724f10b01c46
alex9yust <alex9yust@gmail.com>
2018-07-22 19:19:09 +0000

    kenzo: Initial Treble Support

--- lineage.mk
+++ lineage.mk
@@ -20,6 +20,9 @@ $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit common treble configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
+
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
